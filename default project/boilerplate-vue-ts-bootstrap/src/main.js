e6021babe96fbf22caddb049c3e8ec5bc6fd8dfa
Anjasmara D.S <73101038+anjas-tradaru@users.noreply.github.com>
2021-01-23 22:19:56 +0700

    add boilerplate vue-tailwind-bootstrap and nuxtjs

---
+++ default project/boilerplate-vue-ts-bootstrap/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+
+
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
---
+++ default project/nuxt-boilerplate/pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">nuxt-boilerplate</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="drm-btn color-gray color-outlined size-lg"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="drm-btn color-green color-outlined size-lg"
+        >
+          GitHub
+        </a>
+        <NuxtLink
+          to="/about"
+          class="drm-btn color-green color-outlined size-lg"
+        >
+          About
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'nuxt-class-component';
+
+@Component
+export default class IndexPage extends Vue {}
+</script>
+
+<style lang="scss">
+.container {
+  @apply mx-auto my-0 min-h-screen flex justify-center items-center text-center;
+}
+
+.links,
+.title {
+  @apply mt-5;
+}
+
+.links :not(:first-child) {
+  @apply ml-5;
+}
+</style>
