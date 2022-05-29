82774f6578e3a753aeb0e6771dca1ad19827d01b
anjasmara <anjasmaradwi@refactory.id>
2022-05-29 19:36:33 +0700

    update boilerplate vue tailwinds and bootstrap

---
+++ default project/boilerplate-vue-tailwinds/src/service/collectionUrl/CollectionUrl.js
@@ -0,0 +1,6 @@
+import enviroment from "../enviroment/enviroment";
+const CollectionUrl = {
+  collect: `${enviroment.url}`
+}
+
+export default CollectionUrl
---
+++ default project/boilerplate-vue-tailwinds/src/service/enviroment/enviroment.js
@@ -0,0 +1,4 @@
+const enviroment ={
+  url: ''
+}
+export default enviroment
